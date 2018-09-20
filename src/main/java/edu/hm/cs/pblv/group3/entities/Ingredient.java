fdbae7260f7ca3f6be88a2ed3c13039fa1a8823b
niko- <niko-@10.179.10.238>
2018-09-20 17:00:25 +0200

    implemented database classes with jpa

---
+++ src/main/java/edu/hm/cs/pblv/group3/entities/Ingredient.java
@@ -0,0 +1,44 @@
+package edu.hm.cs.pblv.group3.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Ingredient {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long ingredientId;
+	
+	@OneToMany(mappedBy = "ingredient")
+	private List<Ingredients> ingredients = new ArrayList<>();
+	
+	@Column(length = 128)
+	private String name;
+	
+	
+
+	public Ingredient(List<Ingredients> ingredients, String name) {
+		super();
+		this.ingredients = ingredients;
+		this.name = name;
+	}
+
+	public List<Ingredients> getIngredients() {
+		return ingredients;
+	}
+
+	public String getName() {
+		return name;
+	}
+	
+	
+
+}
---
+++ src/main/java/edu/hm/cs/pblv/group3/repositories/CocktailRepository.java
@@ -0,0 +1,11 @@
+package edu.hm.cs.pblv.group3.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import edu.hm.cs.pblv.group3.entities.Cocktail;
+
+@Repository
+public interface CocktailRepository extends JpaRepository<Cocktail, Long> {
+
+}
