e64c1b48d96af2c314bfa7f639b562b72d4d7946
Gleydar <gleydar@outlook.de>
2018-09-27 16:10:55 +0200

    Lower-Case query

--- src/main/java/edu/hm/cs/pblv/group3/repositories/IngredientRepository.java
+++ src/main/java/edu/hm/cs/pblv/group3/repositories/IngredientRepository.java
@@ -1,20 +1,18 @@
 package edu.hm.cs.pblv.group3.repositories;
 
-import java.util.List;
-import java.util.Set;
-
+import edu.hm.cs.pblv.group3.model.entities.Ingredient;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import edu.hm.cs.pblv.group3.model.entities.Ingredient;
+import java.util.List;
 
 @Repository
 public interface IngredientRepository extends JpaRepository<Ingredient, Long> {
 	
 	public List<Ingredient> findByName(String name);
 	
-	@Query(value = "SELECT DISTINCT name FROM INGREDIENT", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT name FROM ingredient", nativeQuery = true)
 	public List<String> findDistinctName();
 
 }
